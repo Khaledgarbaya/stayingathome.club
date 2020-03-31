de17f9f9a05d96f91582864b63da4316b41bb52e
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-03-31 21:15:36 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home | Staying at home club" />
+    <h1>Staying At Home Club</h1>
+  </Layout>
+)
+
+export default IndexPage
