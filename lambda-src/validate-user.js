89e7e2f648a951657318a4ca18945a451f4f35c0
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-04-07 21:26:03 +0200

    Add hasura auth webhook

---
+++ lambda-src/validate-user.js
@@ -0,0 +1,19 @@
+exports.handler = async (event, context) => {
+  const { identity, user } = context.clientContext
+  if (user) {
+    const userID = user.sub
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        "X-Hasura-User-Id": userID,
+        "X-Hasura-Role": "user",
+      }),
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      "X-Hasura-role": "anonymous",
+    }),
+  }
+}
